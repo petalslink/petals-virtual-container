45f62039da91da2b16071b0202e77652fcc32d39
cdeneux <christophe.deneux@gmail.com>
2015-05-29 08:40:20 +0200

    Add copyright and file headers

--- src/main/model/graph/PetalsRegistry/scripts/env.sh
+++ src/main/model/graph/PetalsRegistry/scripts/env.sh
@@ -1,5 +1,22 @@
 #!/bin/sh -x
-
+#
+# Copyright (c) 2015 Linagora
+#
+# This program/library is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 2.1 of the License, or (at your
+# option) any later version.
+#
+# This program/library is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+# for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program/library; If not, see <http://www.gnu.org/licenses/>
+# for the GNU Lesser General Public License version 2.1.
+#
+#############################################################################
 
 OW2_NEXUS_URL_BASE=http://repository.ow2.org/nexus/service/local/artifact/maven/content
 PETALS_GROUP_ID=org.ow2.petals
